7487e94b5602265c5fdcf709e8d254f82410c50c
JustesII <106695289+JustesII@users.noreply.github.com>
2022-06-01 17:16:01 -0300

    Add files via upload

---
+++ SistemaDeEnergia/src/br/com/ifsc/SistemaDeEnergia/Fatura.java
@@ -0,0 +1,25 @@
+package br.com.ifsc.SistemaDeEnergia;
+
+public class Fatura {
+	
+	private int faturaId;
+	private int mesAno;
+	private UC unidadeConsumidora;
+	private int consumo;
+	private boolean pago;
+	
+	public double calcularValorFatura() {
+		
+		
+	}
+	
+	public void imprimirFatura(fatura vetor[]) {
+		
+	}
+	
+	public String toString() {
+		return "Fatura: " +consumo;
+		
+	}
+
+}
--- SistemaDeEnergia/src/br/com/ifsc/SistemaDeEnergia/UC.java
+++ SistemaDeEnergia/src/br/com/ifsc/SistemaDeEnergia/UC.java
@@ -1,6 +1,6 @@
 package br.com.ifsc.SistemaDeEnergia;
 
-public class UC {
+public class UC extends Fatura {
 
 	private int ucid;
 	private Cliente cliente;
@@ -8,19 +8,59 @@ public class UC {
 	private int leituraAtual;
 	private Endereco endereco;
 	
-	Endereco endereco = new Endereco();
-	
-	
+	@Override
 	public String toString() {
-		return null;
+		return "UC [ucid=" + ucid + ", cliente=" + cliente.toString() + ", leituraAnterior=" + leituraAnterior + ", leituraAtual="
+				+ leituraAtual + ", endereco=" + endereco.toString() + "]";
+	}
+
+	public int getUcid() {
+		return ucid;
+	}
+
+	public void setUcid(int ucid) {
+		this.ucid = ucid;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public int getLeituraAnterior() {
+		return leituraAnterior;
+	}
+
+	public void setLeituraAnterior(int leituraAnterior) {
+		this.leituraAnterior = leituraAnterior;
+	}
+
+	public int getLeituraAtual() {
+		return leituraAtual;
+	}
+
+	public void setLeituraAtual(int leituraAtual) {
+		this.leituraAtual = leituraAtual;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
 	}
 	
 	public int faturar(int leitura) {
-		
+		return Fatura;
 	}
 	
 	public int pegarConsumo() {
-		
+		return consumo;
 	}
+	
 
 }
